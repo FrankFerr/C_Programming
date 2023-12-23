2bab84c4ecc15e85a5714a7ff6572afce2c9f647
Francesco F <ferrantefrancesco878@gmail.com>
2023-12-23 22:05:59 +0100

    esercizio promemoria

---
+++ Cap_13/Remind/readme.txt
@@ -0,0 +1,3 @@
+Francesco Ferrante 23/12/2023
+
+Stampa una serie di promemoria ordinati per giorno
---
+++ Cap_13/Remind/src/remind.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+#include <string.h>
+
+#define MAX_REMINDER 30
+#define MSG_LEN 40
+
+int readLine(char str[], int n);
+
+int main(void){
+
+    printf("*** Stampa una serie di promemoria ordinati per giorno ***\n\n");
+
+    char reminders[MAX_REMINDER][MSG_LEN + 3] = {'\0'}, str[MSG_LEN + 1] = {'\0'};
+    char dayStr[2];
+    int day = 0, nm_reminder = 0, idxInsert;
+
+    while(1){
+        if(nm_reminder == MAX_REMINDER){
+            printf("Non c'e' piu' spazio\n");
+            break;
+        }
+
+        printf("Inserisci giorno e promemoria: ");
+        scanf("%2d", &day);
+
+        if(day < 1){
+            break;
+        }
+
+        sprintf(dayStr, "%2d", day);
+
+        readLine(str, MSG_LEN);
+
+        for(idxInsert = 0; idxInsert < nm_reminder; idxInsert++){
+            if(strcmp(dayStr, reminders[idxInsert]) < 0){
+                break;
+            }
+        }
+
+        for(int i = nm_reminder; i > idxInsert; i--){
+            strcpy(reminders[i], reminders[i - 1]);
+        }
+
+        strcpy(reminders[idxInsert], dayStr);
+        strcat(reminders[idxInsert], str);
+
+        nm_reminder++;
+    }
+
+    printf("\nPromemoria ordinati\n");
+    for(int i = 0; i < nm_reminder; i++){
+        printf(" %s\n", reminders[i]);
+    }
+
+    printf("\n");
+    return 0;
+}
+
+int readLine(char str[], int n){
+    int i = 0;
+    char ch;
+
+    while((ch = getchar()) != '\n')
+        if(i < n)
+            str[i++] = ch;
+
+    str[i] = '\0';
+
+    return i;
+}
