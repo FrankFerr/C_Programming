f35b104441ddc60e5135aa7e670bd643c08b7a4c
Francesco.Ferrante <ferrantefrancesco878@gmail.com>
2024-02-08 23:14:34 +0100

    cap 22 pdp 4

---
+++ Cap_22/pdp4/readme.txt
@@ -0,0 +1,5 @@
+Francesco Ferrante 08/02/2024
+
+a) Conta il numero di caratteri in un file (da riga di comando)
+b) Conta il numero di parole in un file (da riga di comando)
+c) Conta il numero di righe in un file (da riga di comando)
---
+++ Cap_22/pdp4/src/pdp4.c
@@ -0,0 +1,92 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <ctype.h>
+
+int countChar(const char* filename);
+int countWord(const char* filename);
+int countLine(const char* filename);
+
+int main(int argc, char **argv){
+
+    if(argc != 2){
+        printf("Lanciare: canopen <filename>\n");
+        exit(EXIT_FAILURE);
+    }
+
+    int numChar = 0, numWord = 0, numLine = 0;
+
+    numChar = countChar(argv[1]);
+    printf("Totale caratteri: %d\n", numChar);
+    
+    numWord = countWord(argv[1]);
+    printf("Totale parole: %d\n", numWord);
+
+    numLine = countLine(argv[1]);
+    printf("Totale righe: %d\n", numLine);
+
+
+    return 0;
+}
+
+int countChar(const char* filename){
+    int count = 0;
+
+    FILE *fp = fopen(filename, "r");
+
+    if(fp == NULL){
+        printf("Impossibile aprire %s", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    while(fgetc(fp) != EOF)
+        count++;
+
+    fclose(fp);
+
+    return count;
+}
+
+int countWord(const char* filename){
+    int count = 0, ch;
+    FILE *fp = fopen(filename, "r");
+    bool newWord = true;
+
+    if(fp == NULL){
+        printf("Impossibile aprire %s", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    while((ch = fgetc(fp)) != EOF){
+        if(isalpha(ch)){
+            if(newWord){
+                count++;
+                newWord = false;
+            }
+        } else {
+            newWord = true;
+        }
+    }
+
+    fclose(fp);
+
+    return count;
+}
+
+int countLine(const char* filename){
+    int count = 0;
+    FILE *fp = fopen(filename, "r");
+    char s[150 + 1];
+
+    if(fp == NULL){
+        printf("Impossibile aprire %s", filename);
+        exit(EXIT_FAILURE);
+    }
+
+    while(fgets(s, 150, fp) != NULL)
+        count++;
+
+    fclose(fp);
+
+    return count;
+}
